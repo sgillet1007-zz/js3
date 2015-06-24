8f1e7ed3eea50cc76179c2f0a0ae432a6ec39caf
Samuel Gillet <Sam_Home@Samuels-MacBook-Air.local>
2015-06-23 21:26:31 -0600

    Validate phone both dashes

--- js3.js
+++ js3.js
@@ -1,6 +1,6 @@
 var phone = prompt("please enter phone number","xxx-xxx-xxxx");
 
-if (phone.charAt(3)=="-"){
+if ((phone.charAt(3)==="-") && (phone.charAt(7)==="-")){
 	alert("Valid phone number");
 }
 else {
